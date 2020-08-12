c8473eb42413d86d21f6f4b0b5114112c8742c71
ethmcc <64378116+infinj@users.noreply.github.com>
2020-08-12 18:02:48 -0400

    Initial commit

---
+++ precache-manifest.a5beb321a5c9592b2a764c410f734d4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd96325c13fd90a88f22f18a43e2ade",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52aa305b343dc2964940",
+    "url": "/static/css/main.5391dedb.chunk.css"
+  },
+  {
+    "revision": "fb937be412e0a39d0189",
+    "url": "/static/js/2.22bdc401.chunk.js"
+  },
+  {
+    "revision": "52aa305b343dc2964940",
+    "url": "/static/js/main.40966703.chunk.js"
+  },
+  {
+    "revision": "783b15915ddc7c842ac4",
+    "url": "/static/js/runtime-main.df74dd54.js"
+  }
+]);
